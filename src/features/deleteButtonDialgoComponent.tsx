2868da8317737591cb527987362cf865877ea6a8
Raviteja Penugonda <raviteja@eazydiner.com>
2021-04-02 00:35:35 +0530

    feat: Delete dialogBox component added
    - delete fun added using dialogBox

---
+++ src/features/deleteButtonDialgoComponent.tsx
@@ -0,0 +1,77 @@
+import React, { FunctionComponent, useState } from "react";
+import Dialog from "@material-ui/core/Dialog";
+import { Button, DialogActions, Typography } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import MuiDialogActions from "@material-ui/core/DialogActions";
+import { Theme, withStyles } from "@material-ui/core/styles";
+
+interface Props {
+  userEmail: string;
+}
+export const DeleteButtonDialogComponent: FunctionComponent<Props> = (
+  props
+) => {
+  const [open, setOpen] = useState(false);
+  const [del, setDel] = useState("");
+
+  const DialogActions = withStyles((theme: Theme) => ({
+    root: {
+      margin: 4,
+      padding: theme.spacing(4),
+    },
+  }))(MuiDialogActions);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleButtonClick = (val: string) => {
+    setDel(val);
+    console.log(props.userEmail);
+    setOpen(false);
+    fetch("/records/" + props.userEmail, {
+      method: "DELETE",
+    }).catch((err) => {
+      console.log(err);
+    });
+  };
+
+  return (
+    <>
+      <Button
+        size="small"
+        color="secondary"
+        variant="contained"
+        startIcon={<DeleteIcon />}
+        onClick={handleClickOpen}
+      >
+        Delete
+      </Button>
+      <Dialog open={open} onClose={handleClose} maxWidth="lg">
+        <Typography variant="h6">Confirm</Typography>
+        <DialogActions>
+          <Button
+            size="small"
+            variant="outlined"
+            color="primary"
+            onClick={() => handleButtonClick("yes")}
+          >
+            Yes
+          </Button>
+          <Button
+            size="small"
+            variant="outlined"
+            color="primary"
+            onClick={() => handleButtonClick("no")}
+          >
+            No
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
--- src/features/forms/viewRecordComponent.tsx
+++ src/features/forms/viewRecordComponent.tsx
@@ -1,134 +1,125 @@
-import React,{useEffect, useState} from 'react'
-import {UserForm} from './reducer'
-import {Button, Card, CardActions, CardContent, CardHeader, GridList, GridListTile} from '@material-ui/core'
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
-import axios from 'axios';
-import DeleteIcon from '@material-ui/icons/Delete'
-import CreateIcon from '@material-ui/icons/Create'
+import React, { useEffect, useState } from "react";
+import { UserForm } from "./reducer";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  GridList,
+  GridListTile,
+} from "@material-ui/core";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import axios from "axios";
+import DeleteIcon from "@material-ui/icons/Delete";
+import CreateIcon from "@material-ui/icons/Create";
+import { DeleteButtonDialogComponent } from "../deleteButtonDialgoComponent";
 
+export const ViewRecordComponent = () => {
+  let data_arr: UserForm[] = [];
+  const [allUsersData, setAllUsersData] = useState<UserForm[]>([]);
+  useEffect(() => {
+    axios.get("/records").then((res) => {
+      const data: Array<any> = res.data;
+      data.map((user) => {
+        let newUser: UserForm = {
+          firstName: user.firstName,
+          email: user.email,
+          gender: user.gender,
+          mobileNumber: user.mobileNumber,
+          c: user.c,
+          c_plus: user.c_plus,
+          python: user.python,
+          userImage: user.image,
+        };
+        data_arr.push(newUser);
+      });
+      setAllUsersData(data_arr);
+    });
+  }, []);
+  // const loadData = () =>{
+  //     const localStorageVal = localStorage.getItem('usersData')
+  //     if(localStorageVal){
+  //         let arr: Array<UserForm> = JSON.parse(localStorageVal);
+  //         arr.map((user) => {
+  //             allUsersData.push(user);
+  //         });
+  //     }
+  // }
 
-export const ViewRecordComponent = () =>{
-    let data_arr:UserForm[] = [];
-    const [allUsersData, setAllUsersData] = useState<UserForm[]>([])
-    useEffect(()=>{
-        axios.get('/records').then(res => {
-            const data:Array<any> = res.data
-            data.map((user) =>{
-                let newUser:UserForm = {
-                    firstName: user.firstName,
-                    email: user.email,
-                    gender:user.gender,
-                    mobileNumber:user.mobileNumber,
-                    c:user.c,
-                    c_plus:user.c_plus,
-                    python:user.python,
-                    userImage:user.image
-                }
-                data_arr.push(newUser)
-            })
-            setAllUsersData(data_arr)
-        })
-    },[])
-    // const loadData = () =>{
-    //     const localStorageVal = localStorage.getItem('usersData')
-    //     if(localStorageVal){
-    //         let arr: Array<UserForm> = JSON.parse(localStorageVal);
-    //         arr.map((user) => {
-    //             allUsersData.push(user);
-    //         });
-    //     }
-    // }
+  // loadData()
 
-    // loadData()
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      root: {
+        display: "flex",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+        overflow: "hidden",
+        backgroundColor: theme.palette.background.paper,
+      },
+      gridList: {
+        width: 1000,
+        height: 600,
+      },
+    })
+  );
+  const useStylesForCard = makeStyles((theme: Theme) =>
+    createStyles({
+      root: {
+        maxWidth: 375,
+      },
+      media: {
+        height: 0,
+        paddingTop: "56.25%", // 16:9
+      },
+    })
+  );
 
-    const useStyles = makeStyles((theme: Theme) =>
-        createStyles({
-            root: {
-                display: 'flex',
-                flexWrap: 'wrap',
-                justifyContent: 'space-around',
-                overflow: 'hidden',
-                backgroundColor: theme.palette.background.paper,
-            },
-            gridList: {
-                width: 1000,
-                height: 600,
-            },
-        }),
-    );
-    const useStylesForCard = makeStyles((theme: Theme) =>
-        createStyles({
-            root: {
-                maxWidth: 375,
-            },
-            media: {
-                height: 0,
-                paddingTop: '56.25%', // 16:9
-            },
-        }),
-    );
+  const classesForCard = useStylesForCard();
+  const classes = useStyles();
 
-    const classesForCard = useStylesForCard();
-    const classes = useStyles()
+  const handleEditUser = (userEmail: string) => {
+    console.log(userEmail);
+  };
 
-    const handleEditUser = (userEmail:string) =>{
-        console.log(userEmail)
-    }
-
-    const handleDeleteUser = (userEmail:string) =>{
-        console.log(userEmail)
-        fetch('/records/'+userEmail,{
-            method:'DELETE'
-        }).catch(err => {console.log(err)})
-    }
-    return(
-        <>
-            <h4>Users Records View</h4>
-            {console.log(allUsersData)}
-            <div className={classes.root}>
-                <GridList cellHeight={700} className={classes.gridList} cols={3}>
-                    {allUsersData.map((user) => (
-                        <GridListTile key={user.email} cols={1}>
-                            <Card className={classesForCard.root} variant="outlined">
-                                <CardHeader 
-                                    title={user.firstName}
-                                    subheader = {user.email}
-                                />
-                                {/* <CardMedia src={`data:image/jpeg/png;base64,${user.userImage}`}/> */}
-                                {/* <img src={`data:image/jpeg/png;base64,${user.userImage}`} alt="Image not loaded" width="275" height="250"/> */}
-                                <img src="" alt="Image not loaded" width="275" height="250"/>
-                                <CardContent>
-                                    <h4>{user.gender}</h4>
-                                    <h4>{user.mobileNumber}</h4>
-                                    {user.c === true?<h4>C</h4>:''}
-                                    {user.c_plus === true?<h4>C_Plus</h4>:''}
-                                    {user.python === true?<h4>Python</h4>:''}
-                                </CardContent>
-                                <CardActions>
-                                    <Button 
-                                        size="small" 
-                                        color="primary" 
-                                        variant="contained" 
-                                        startIcon={<CreateIcon/>}
-                                        onClick={() => handleEditUser(user.email)}
-                                    >
-                                        Edit
-                                    </Button>
-                                    <Button 
-                                        size="small" 
-                                        color="secondary" 
-                                        variant="contained" 
-                                        startIcon={<DeleteIcon/>}
-                                        onClick = {() => handleDeleteUser(user.email)}
-                                    >
-                                        Delete
-                                    </Button>
-                                </CardActions>
-                            </Card>
-                        </GridListTile>
-                    ))}
-                </GridList>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <h4>Users Records View</h4>
+      {console.log(allUsersData)}
+      <div className={classes.root}>
+        <GridList cellHeight={700} className={classes.gridList} cols={3}>
+          {allUsersData.map((user) => (
+            <GridListTile key={user.email} cols={1}>
+              <Card className={classesForCard.root} variant="outlined">
+                <CardHeader title={user.firstName} subheader={user.email} />
+                {/* <CardMedia src={`data:image/jpeg/png;base64,${user.userImage}`}/> */}
+                {/* <img src={`data:image/jpeg/png;base64,${user.userImage}`} alt="Image not loaded" width="275" height="250"/> */}
+                <img src="" alt="Image not loaded" width="275" height="250" />
+                <CardContent>
+                  <h4>{user.gender}</h4>
+                  <h4>{user.mobileNumber}</h4>
+                  {user.c === true ? <h4>C</h4> : ""}
+                  {user.c_plus === true ? <h4>C_Plus</h4> : ""}
+                  {user.python === true ? <h4>Python</h4> : ""}
+                </CardContent>
+                <CardActions>
+                  <Button
+                    size="small"
+                    color="primary"
+                    variant="contained"
+                    startIcon={<CreateIcon />}
+                    onClick={() => handleEditUser(user.email)}
+                  >
+                    Edit
+                  </Button>
+                  <DeleteButtonDialogComponent userEmail={user.email} />
+                </CardActions>
+              </Card>
+            </GridListTile>
+          ))}
+        </GridList>
+      </div>
+    </>
+  );
+};
